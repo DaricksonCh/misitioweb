6785743bfb0c677420a9a4b35831de78ef7a1807
DaricksonCh <darickson.stiven96@gmail.com>
2022-11-03 11:36:54 -0500

    s

---
+++ js/menuSubItem.js
@@ -0,0 +1,12 @@
+
+let enviar = document.getElementById("enviar");
+
+enviar.addEventListener ('click', function() {
+    alert('Has dado click en enviar');
+});
+
+let option2 = document.getElementById("option-2");
+
+option2.addEventListener('click', function(){
+    alert('Has dado click en Nosotros');
+})

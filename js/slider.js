3299aa54ca1d1a2c8db89df897319eb8a98eb67f
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2023-05-04 11:45:51 -0500

    slider

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+let imgs = document.querySelector(".imgs");
+let arrowRight = document.getElementById("arrowRight");
+let arrowLeft = document.getElementById("arrowLeft");
+let imgValorant1 = document.getElementById("imgValorant1");
+
+
+
+
+
+    // arrowLeft.addEventListener("click", function(){
+    //     imgValorant1.classList.remove("img-valorant1");
+    //     imgValorant1.classList.add("img-valorant-after");
+    // });
+
+    // arrowRight.addEventListener("click", function(){
+    //     arrowRight.classList.remove("img-valorant1-after")
+    // });

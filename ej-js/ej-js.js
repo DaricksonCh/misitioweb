75269691998045404e71bb44330248f4a00da874
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2023-06-22 11:44:33 -0500

    time

---
+++ ej-js/ej-js.js
@@ -0,0 +1,68 @@
+/* El siguiente codigo se ejecuta una vez cargue el documento html */
+
+// alert("hola");
+
+/* Activando el modo Stric */
+
+// "use strict";
+
+// nombreAprendiz = "Darickson";
+
+// alert(nombreAprendiz);
+
+/* Ejercicio N.3 crear dos variables de tipo de string (Nombres y apellidos) y las mostramos en consola */
+
+// let nombre = "Darickson";
+// let apellido = "Chacon"
+
+// console.log(nombre + " "+ apellido);
+
+/* Ejercicio N.4 convercion implicita de variables */
+
+// let num1 = 5;
+// let num2 = "3";
+// let resultado = num1 + num2;
+
+
+/* Ejercicio N.5 convercion explicita de variables en consolas */
+
+// let num3Int = parseInt(prompt("Escriba un numero"));
+// console.log(num3Int);
+
+// let num3Str = num3Int.toString();
+
+
+// let num4Int = parseFloat(prompt("Escriba un numero"));
+// console.log(num4Int);
+
+// let num5Int = parseInt(num4Int);
+
+
+// let num4Int = parseFloat(prompt("Escriba un numero"));
+// console.log(num4Int);
+
+// let num5Int = Math.ceil(num4Int);
+// console.log(num5Int);
+// let num6Int = Math.floor(num4Int);
+// console.log(num6Int);
+
+
+/* limitaciones de variables */
+
+// let num1 = 8;
+// let num2 = 5;
+
+// console.log("Suma de los dos numeros es " + (num1 + num2));
+// console.log("Resta de los dos numeros es " + (num2 - num1));
+// console.log("Multi de los dos numeros es " + (num2 * num1));
+// console.log("Residuo de los dos numeros es " + (num1 % num2));
+// console.log("Divi de los dos numeros es " + Math.round((num1 / num2)));
+
+
+console.log("Hola")
+
+
+
+
+
+

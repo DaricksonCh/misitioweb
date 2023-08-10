6de3f45a7775f731332a610f34c8223fcaa54062
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2023-08-10 11:32:03 -0500

    ana

--- js/homecenter.js
+++ js/homecenter.js
@@ -13,3 +13,18 @@ lupa.addEventListener("click", function() {
         containerMobile.style.display = 'flex'; 
     });
 });
+
+containerXAnna = document.getElementById('containerXAnna');
+containerAna = document.getElementById('containerAna');
+containerChat = document.getElementById('containerChat');
+
+
+containerAna.addEventListener("click",function(){
+    containerChat.style.display = 'none';
+    containerXAnna.style.display = 'flex';
+    containerXAnna.addEventListener("click",function(){
+        containerXAnna.style.display = 'none';
+        containerChat.style.display = 'flex';
+    })
+})
+

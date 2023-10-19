7767ae3ef7350fcf4c54d0c5ed845d06311198ac
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2023-10-19 10:50:51 -0500

    cpu

--- js/triki.js
+++ js/triki.js
@@ -37,3 +37,29 @@ bodyLeft.addEventListener('click', function () {
     iconc.style.color = '#1A2A33';
   }
 });
+
+
+function cpu(){
+  let timerInterval
+  Swal.fire({
+    title: 'Trabajando en ello...',
+    html: 'Descargando CPU <b></b> milliseconds.',
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading()
+      const b = Swal.getHtmlContainer().querySelector('b')
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft()
+      }, 100)
+    },
+    willClose: () => {
+      clearInterval(timerInterval)
+    }
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log('I was closed by the timer')
+    }
+  })
+}
+

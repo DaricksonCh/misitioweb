0801ad4a89121e82f8f2cfa28d9840ca8c94eefa
DaricksonCh <darickson.stiven96@gmail.com>
2023-11-01 17:43:04 -0500

    cohete

--- js/ovni.js
+++ js/ovni.js
@@ -1,14 +1,29 @@
-let allAliens = document.querySelectorAll(".aliens");
-
+let cohete = document.querySelector(".cohete");
+let cohetePositionHeight = 18;
+let cohetePositionWidth = 169;
+let coheteHeight = cohete.clientHeight;
+let coheteWidth = cohete.clientWidth;
+let containerHeight = document.querySelector(".container").clientHeight;
+let containerWidth = document.querySelector(".container").clientWidth;
 
 window.addEventListener("keydown", (e) => {
-  for(let i = 0; i < allAliens.length; i++){
-    if (e.key == "ArrowDown") {
-      allAliens[i].classList.remove("aliens")
-      allAliens[i].classList.add("aliensR")
-    }else if(e.key == "ArrowUp"){
-      allAliens[i].classList.add("aliens")
-      allAliens[i].classList.remove("aliensR")
-    }
+  if (e.key == "ArrowUp" && cohetePositionHeight < (containerHeight - coheteHeight)) {
+    cohetePositionHeight += 2;
+    cohete.style.bottom = cohetePositionHeight + "vh";
+  } else if (e.key == "ArrowDown" && cohetePositionHeight > 0) {
+    cohetePositionHeight -= 2;
+    cohete.style.bottom = cohetePositionHeight + "vh";
+  } else if (e.key == "ArrowRight" && cohetePositionWidth < (containerWidth - coheteWidth)) {
+    cohetePositionWidth += 2;
+    cohete.style.left = cohetePositionWidth + "vh";
+    cohete.style.transform = 'scaleX(1)';
+  } else if (e.key == "ArrowLeft" && cohetePositionWidth > 0) {
+    cohetePositionWidth -= 2;
+    cohete.style.left = cohetePositionWidth + "vh";
+    cohete.style.transform = 'scaleX(-1)';
+  } else if (e.key == "ArrowRight" && cohetePositionWidth >= containerWidth) {
+    cohetePositionWidth = containerWidth - coheteWidth;
+    cohete.style.left = cohetePositionWidth + "vh";
+    cohete.style.transform = 'scaleX(1)';
   }
 });
